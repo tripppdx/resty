6ede6a3eb35643714839c7b17203c6bf1840a8a0
Tripp Lucas <tripppdx@gmail.com>
2021-11-22 17:02:15 -0800

    convert to functions

--- src/app.js
+++ src/app.js
@@ -4,13 +4,12 @@ import './app.scss';
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Form from './components/form/Form';
+import Results from './components/results/Results';
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -19,17 +18,17 @@ class App extends React.Component {
     };
   }
 
-  callApi = (requestParams) => {
+  callApi = requestParams => {
     // mock output
     const data = {
       count: 2,
       results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
       ],
     };
-    this.setState({data, requestParams});
-  }
+    this.setState({ data, requestParams });
+  };
 
   render() {
     return (
