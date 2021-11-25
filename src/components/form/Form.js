4e8286f67af223f2a226f6b6205d897e237fe891
Tripp Lucas <tripppdx@gmail.com>
2021-11-24 17:03:55 -0800

    update form, add api

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,24 +1,40 @@
 import './form.scss';
+import { useState } from 'react';
 
 function Form(props) {
   function handleSubmit(e) {
     e.preventDefault();
     const formData = {
-      method: 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: method,
+      url: url,
     };
     props.handleApiCall(formData);
   }
+
+  const [url, setUrl] = useState('');
+
+  const handleSetUrl = e => {
+    let { value } = e.target;
+    setUrl(value);
+  };
+
+  const [method, setMethod] = useState('');
+
+  const handleSetMethod = e => {
+    let { id } = e.target;
+    setMethod(id);
+    console.log(id);
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
           <span>URL: </span>
-          <input name="url" type="text" />
+          <input onChange={handleSetUrl} name="url" type="text" />
           <button type="submit">GO!</button>
         </label>
-        <label className="methods">
+        <label onClick={handleSetMethod} className="methods">
           <span id="get">GET</span>
           <span id="post">POST</span>
           <span id="put">PUT</span>
